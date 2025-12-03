7383d961eb2f93b2c41d84ba85254b8bbfaed098
robertopfneto <neto2014roberto@gmail.com>
2025-12-03 09:20:04 -0400

    removendo autenticação

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Register from "./components/Register.jsx";
 import Root from "./Root.jsx";
-import PrivateRoute from "./components/PrivateRoute";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
-       
-        <Route path="/" element={<Login />} />
-        <Route path="/cadastro" element={<Register />} />
-
-       
-        <Route
-          path="/dashboard"
-          element={<PrivateRoute element={<Root />} />}
-        />
+        <Route path="/" element={<Root />} />
+        <Route path="/dashboard" element={<Root />} />
       </Routes>
     </Router>
   </React.StrictMode>
