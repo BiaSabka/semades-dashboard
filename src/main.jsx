2a5416496c538df6ddb046e802370f3fac78183b
robertopfneto <neto2014roberto@gmail.com>
2025-12-03 09:22:05 -0400

    removendo auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register.jsx";
 import Root from "./Root.jsx";
+import PrivateRoute from "./components/PrivateRoute";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Root />} />
+       
+       <Route path="/" element={<Root />} />
         <Route path="/dashboard" element={<Root />} />
-      </Routes>
+
+
+        </Routes>
     </Router>
   </React.StrictMode>
 );
